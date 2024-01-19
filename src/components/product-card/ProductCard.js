2f3b0b8ba79717542b37bcf39e2cb88f0f1d8932
Mehad Mushtaq <mehadmushtaq@gmail.com>
2024-01-19 18:07:35 +0500

    Revert "card edited"
    This reverts commit 6e29cc57add657a7dea09e6b4ad5f963d24e1ff5.

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, Rating, Box, useTheme } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Rating, Box, useTheme } from "@mui/material";
 
 export function ProductCard({ item }) {
   const theme = useTheme();
@@ -12,60 +12,60 @@ export function ProductCard({ item }) {
     <Card
       elevation={0}
       sx={{
-        position: 'relative',
-        overflow: 'visible',
-        // border: "1px solid lightgrey",
+        position: "relative",
+        overflow: "visible",
+        border: "1px solid lightgrey",
       }}
     >
       {/* SALE BADGE */}
       {item?.salePrice ? (
         <Box
           sx={{
-            width: '50px',
-            height: '50px',
-            backgroundColor: 'red',
-            borderRadius: '50%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'absolute',
-            zIndex: '1',
-            top: '-10px',
-            right: '-10px',
+            width: "50px",
+            height: "50px",
+            backgroundColor: "red",
+            borderRadius: "50%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            zIndex: "1",
+            top: "-10px",
+            right: "-10px",
           }}
         >
           <Typography>Sale</Typography>
         </Box>
       ) : (
-        ' '
+        " "
       )}
 
       <CardActionArea>
         <CardMedia
-          component='img'
+          component="img"
           image={item.url}
-          alt='green iguana'
+          alt="green iguana"
           sx={{
-            height: { xs: '12rem', md: '20rem', borderRadius: '0.5rem' },
+            height: { xs: "12rem", md: "20rem" },
           }}
         />
         <CardContent
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
           <Typography
             sx={{
-              typography: { xs: 'subtitle2', md: 'body1' },
+              typography: { xs: "subtitle2", md: "body1" },
             }}
           >
             {item.name}
           </Typography>
           <Rating
-            name='read-only'
+            name="read-only"
             defaultValue={2.5}
             precision={0.5}
             readOnly
@@ -73,12 +73,12 @@ export function ProductCard({ item }) {
 
           {item.salePrice ? (
             <>
-              <Typography variant='body1'>
+              <Typography variant="body1">
                 {item.salePrice}
                 <span
                   style={{
-                    textDecoration: 'line-through',
-                    marginLeft: '0.5rem',
+                    textDecoration: "line-through",
+                    marginLeft: "0.5rem",
                   }}
                 >
                   {item.price}
@@ -86,7 +86,7 @@ export function ProductCard({ item }) {
               </Typography>
             </>
           ) : (
-            <Typography variant='body1'>{item.price}</Typography>
+            <Typography variant="body1">{item.price}</Typography>
           )}
         </CardContent>
       </CardActionArea>
